9e9536d4d54ee0a981ac80025f63b2da71a3f0b6
Claire Ricca <clair@Claires-HP.localdomain>
2026-04-24 21:22:05 -0600

    basic algo description add to radnom move strat

--- src/main/java/tictactoe/strategies/RandomMoveStrategy.java
+++ src/main/java/tictactoe/strategies/RandomMoveStrategy.java
@@ -4,7 +4,8 @@ import tictactoe.*;
 
 import java.util.List;
 import java.util.Random;
-
+/*This creates a list of all the open positions on the board and uses rand
+* to select a random index of that list (inclusive size of the list so we do have a seg fault*/
 public class RandomMoveStrategy implements MoveStrategy {
     private final Random rand = new Random();
 
